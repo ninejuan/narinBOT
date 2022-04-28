9bacbb1946e2bd4ea3e238cca6e70242ab03f3b7
주안 이 <ninejuan@protonmail.com>
2022-04-29 00:41:05 +0900

    관리 명령어 제작 중 에러 발생 추정

--- Program.cs
+++ Program.cs
@@ -58,8 +58,8 @@ namespace DiscordBot
 
             int pos = 0;
 
-            //메시지 앞에 !이 달려있지 않고, 자신이 호출된게 아니거나 다른 봇이 호출했다면 취소
-            if (!(message.HasStringPrefix(prefix, ref pos) ||
+            //메시지 앞에 접두사가 달려있지 않고, 자신이 호출된게 아니거나 다른 봇이 호출했다면 취소
+            if (!(message.HasStringPrefix("나린아 ", ref pos) ||
              message.HasMentionPrefix(client.CurrentUser, ref pos)) ||
               message.Author.IsBot)
                 return;
@@ -86,3 +86,76 @@ namespace DiscordBot
         }
     }
 }
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBot
+{
+    class Progrram
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "OTY4Nzg0MTExNTg0MDM0ODQ2.Ymj4gw.TwHRwjTs84zhHaEslJyszySgrAQ";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("나린아 ", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+}*/
