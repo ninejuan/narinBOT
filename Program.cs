182980c08cacae556417462626c5df73870b2b2a
Nine <79080468+john0128@users.noreply.github.com>
2022-05-17 15:59:27 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ namespace DiscordBot
             client.Log += OnClientLogReceived;
             commands.Log += OnClientLogReceived;
 
-            await client.LoginAsync(TokenType.Bot, "OTY5ODI5NTA5MzU0Mzc3MjQ2.YmzGHQ.PTTzBaTfbRdCmCgfBjhXuvxYaGc"); //봇의 토큰을 사용해 서버에 로그인
+            await client.LoginAsync(TokenType.Bot, "토오오큰"); //봇의 토큰을 사용해 서버에 로그인
             await client.StartAsync();                         //봇이 이벤트를 수신하기 시작
 
             client.MessageReceived += OnClientMessage;         //봇이 메시지를 수신할 때 처리하도록 설정
